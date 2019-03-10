c7fa67d3797975c24567618992e8c2dd8c441f4e
Thomas Toye <thomas@toye.io>
2019-03-10 21:10:25 +0100

    Show log in / log out depending on auth status

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import UserCredential = firebase.auth.UserCredential;
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private angularFireAuth: AngularFireAuth) {}
+
+  isLoggedIn$(): Observable<boolean> {
+    return this.angularFireAuth.authState.pipe(map(user => !!user));
+  }
 
   login(email: string, password: string): Promise<UserCredential> {
     return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
@@ -16,5 +22,9 @@ export class AuthService {
 
   sendPasswordReset(email: string): Promise<void> {
     return this.angularFireAuth.auth.sendPasswordResetEmail(email);
+  }
+
+  logout(): Promise<void> {
+    return this.angularFireAuth.auth.signOut();
   }
 }
--- src/app/modules/nav/nav/nav.component.ts
+++ src/app/modules/nav/nav/nav.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
+import { AuthService } from '../../../auth.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
+
+  destroy$ = new Subject<void>();
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private auth: AuthService,
+  ) {
 
   }
 
   ngOnInit(): void {
+    this.isLoggedIn$ = this.auth.isLoggedIn$().pipe(takeUntil(this.destroy$));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+  }
+
+  logout() {
+    this.auth.logout();
   }
 }
